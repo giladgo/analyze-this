96ddfe93d56b4ac74380adc3ed79c5e9061a52cb
Gilad Goldberg <ggoldberg@paypal.com>
2016-09-19 10:50:10 +0300

    Remove tests

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,12 @@ module AnalyzeThis
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+		# Tests in a Hackathon? NOPE
+		config.generators do |g|
+		    g.test_framework  :rspec, :fixture => false
+		    g.view_specs      false
+		    g.helper_specs    false
+		end
   end
 end
