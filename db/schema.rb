e66330960150e53cbff3405faf1b0762d31ac190
Ido Feins <ifeins@gmail.com>
2016-09-21 08:42:16 +0300

    Switched the project to use postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160919075631) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -23,7 +26,7 @@ ActiveRecord::Schema.define(version: 20160919075631) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["category_id"], name: "index_merchants_on_category_id"
+    t.index ["category_id"], name: "index_merchants_on_category_id", using: :btree
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -39,8 +42,8 @@ ActiveRecord::Schema.define(version: 20160919075631) do
     t.date     "txn_date"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.index ["merchant_id"], name: "index_transactions_on_merchant_id"
-    t.index ["user_id"], name: "index_transactions_on_user_id"
+    t.index ["merchant_id"], name: "index_transactions_on_merchant_id", using: :btree
+    t.index ["user_id"], name: "index_transactions_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +52,7 @@ ActiveRecord::Schema.define(version: 20160919075631) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "merchants", "categories"
+  add_foreign_key "transactions", "merchants"
+  add_foreign_key "transactions", "users"
 end
