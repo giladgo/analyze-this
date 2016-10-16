10caef637c93992f98b028aee63201f7bd1257df
Ido Feins <ifeins@gmail.com>
2016-10-16 19:08:03 +0300

    Fixed identity token response parsing.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,21 +2,24 @@ class UserController < ApplicationController
 
   def create
     id_token = params[:id_token]
-    response = GoogleApi.new.verify_id_token(id_token)
-    if response.code == 200
-      email = response.body['email']
-      user = User.find_or_create_by!(email: email) do |user|
-        user.name = response.body['name']
-      end
-
-      respond_to do |format|
-        format.json {render json: user}
-      end
-    else
-      respond_to do |format|
-        format.json {render json: response.body, status: response.code}
-      end
+    user = User.find_by(id_token: id_token)
+    if user.present?
+      render json: user
+      return
     end
+
+    response = GoogleApi.new.verify_id_token(id_token)
+    if response.code != 200
+      Rails.logger.error("Could not verify id token: #{response.body}")
+      render json: response.body, status: response.code
+      return
+    end
+
+    user = User.find_or_create_by!(id_token: id_token) do |user|
+      user.email = response.parsed_response['email']
+      user.name = response.parsed_response['name']
+    end
+    render json: user
   rescue Exception => e
     Rails.logger.error("#{e.message}\n " + e.backtrace.join("\n "))
     respond_to do |format|
